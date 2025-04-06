9cf4ba7ce49ba8e829d929c520ff5f19909f01ea
Bhargav S <bhargavbharu447@gmail.com>
2025-04-06 12:35:41 +0530

    added LinearSearch method

--- src/functions/overLoading.java
+++ src/functions/overLoading.java
@@ -1,10 +1,18 @@
 package functions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class overLoading {
     public static void main(String[] args) {
         // overloading means : same function name and different no of parameters and their type
-        System.out.println(sum(2, 3));
-        System.out.println(sum(2 , 3, 4));
+//        System.out.println(sum(2, 3));
+//        System.out.println(sum(2 , 3, 4));
+
+       
+
     }
 
     static int sum(int a , int b) {
---
+++ src/linearSearch/LinearSearch.java
@@ -0,0 +1,46 @@
+package linearSearch;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {10 , 20 , 30, 40 , 50 };
+       int  target = 30;
+
+       boolean exist = ls(arr, target);
+       if(exist == true) {
+           System.out.println("element exists");
+       } else {
+           System.out.println("element doesnot exists");
+       }
+
+        System.out.println("element exists " + ls2(arr, target));
+    }
+
+    static boolean ls(int[] arr , int target) {
+        boolean exist = false;
+        for (int i=0;i<arr.length;i++) {
+            if(target == arr[i]) {
+                exist = true;
+                break;
+            }
+            else{
+                continue;
+            }
+        }
+
+        return exist;
+    }
+
+    static int ls2(int[] arr , int target) {
+        boolean exist = false;
+        for (int j : arr) {
+            if (target == j) {
+                return j;
+            } else {
+                continue;
+            }
+        }
+
+        return -1;
+    }
+
+}
