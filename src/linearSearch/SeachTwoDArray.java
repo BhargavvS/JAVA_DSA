4193c857a77804ff155b656d0d5dacc33c28d2ed
Bhargav S <bhargavbharu447@gmail.com>
2025-04-13 14:13:55 +0530

    added FindMax method in 2DArray

--- src/linearSearch/SeachTwoDArray.java
+++ src/linearSearch/SeachTwoDArray.java
@@ -10,6 +10,8 @@ public class SeachTwoDArray {
         };
         int target = 34;
         System.out.println("target element exist: "+ Search(nums,target));
+
+        System.out.println("Max value of the array: " + FindMax(nums));
     }
     static boolean Search(int[][] nums , int target) {
         for(int i=0;i<nums.length;i++) {
@@ -19,6 +21,16 @@ public class SeachTwoDArray {
         }
 
         return false;
+    }
+
+    static int FindMax(int[][] nums) {
+        int max= Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                if(nums[i][j] > max) max= nums[i][j];
+            }
+        }
+        return max;
     }
 }
 
